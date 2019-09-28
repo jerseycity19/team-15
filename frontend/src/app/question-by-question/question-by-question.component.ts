f2e5e0ffc51ff5894d2f9bb7c8b49c2f644a9ade
nataliethomas9 <nataliethomas979@gmail.com>
2019-09-28 01:49:47 -0400

    http request

--- frontend/src/app/question-by-question/question-by-question.component.ts
+++ frontend/src/app/question-by-question/question-by-question.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {RadioButtonModule} from 'primeng/radiobutton';
 import { FormsServiceService } from '../services/forms-service.service';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-question-by-question',
@@ -11,9 +12,12 @@ export class QuestionByQuestionComponent  {
   selectedValue: string = 'val1';
   value = 50;
 
-  constructor(private formsService: FormsServiceService) {}
+  constructor(private formsService: FormsServiceService, private httpService: HttpService) {}
 
   onSubmit() {
     this.formsService;
+    this.httpService.sendData(this.formsService.person)
+      .subscribe(data => {
+    });
   }
 }
