d5e243567be737f45ffb34646d16b2a00fd22c77
Winston <wzhou39@terpmail.umd.edu>
2019-09-28 06:05:46 -0400

    Frontend

--- frontend/src/app/question-by-question/question-by-question.component.ts
+++ frontend/src/app/question-by-question/question-by-question.component.ts
@@ -8,12 +8,26 @@ import { FormsServiceService } from '../services/forms-service.service';
   styleUrls: ['./question-by-question.component.css']
 })
 export class QuestionByQuestionComponent  {
-  selectedValue: string = 'val1';
+  selectedValue: string;
+  selectedValue2: string;
+  selectedValue3: string;
+  selectedValue4: string;
+  selectedValue5: string;
+  selectedValue6: string;
+  selectedValue7: string;
+
   value = 50;
 
   constructor(private formsService: FormsServiceService) {}
 
   onSubmit() {
     this.formsService;
+    console.log(this.selectedValue);
+    console.log(this.selectedValue2);
+    console.log(this.selectedValue3);
+    console.log(this.selectedValue4);
+    console.log(this.selectedValue5);
+    console.log(this.selectedValue6);
+    console.log(this.selectedValue7);
   }
 }
