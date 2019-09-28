3c8a85b47d5f0d5fc655789a5c088d27beeb575c
unknown <jennyma@princeton.edu>
2019-09-28 09:51:05 -0400

    added controller class to query data from sql

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-
-
-
 public class Controller {
     public String id;
     public int age;
@@ -21,24 +17,78 @@ public class Controller {
     public String q5;
     public String q6;
     public String q7;
- 
-    
-    public Controller(String id_, String user_, int age_, String gender_, String country_, String language_, String employment_, String disciple_, String sensitivity_, String q1_, String q2_, String q3_, String q4_, String q5_, String q6_, String q7_){
-        id=id_;
-        user = user_;
-        age=age_;
-        gender=gender_;
-        country=country_;
-        language=language_;
-        employment=employment_;
-        disciple=disciple_;
-        sensitivity=sensitivity_;
-        q1=q1_;
-        q2=q2_;
-        q3=q3_;
-        q4=q4_;
-        q5=q5_;
-        q6=q6_;
-        q7=q7_;
+
+
+    public Controller() {
+        createTable();
+    }
+
+    public static void createTable() {
+        String url = "jdbc:sqlite:sqlite/JTP.db";
+
+        String sql = "CREATE TABLE results (" +
+                "'id' varchar(30) NOT NULL, " +
+                "'user' varchar(13) NOT NULL, " +
+                "'age' varchar(5) NOT NULL, " +
+                "'gender' char(1) NOT NULL, " +
+                "'country' varchar(28) NOT NULL, " +
+                "'language' varchar(30) NOT NULL, " +
+                "'employment' varchar(37) NOT NULL, " +
+                "'disciple' varchar(19) NOT NULL, " +
+                "'sensitivity' varchar(24) NOT NULL, " +
+                "'q1' varchar(12) NOT NULL, " +
+                "'q2' varchar(10) NOT NULL, " +
+                "'q3' varchar(12) NOT NULL, " +
+                "'q4' varchar(29) NOT NULL, " +
+                "'q5' varchar(8) NOT NULL, " +
+                "'q6' varchar(8) NOT NULL, " +
+                "'q7' varchar(14) NOT NULL);\n";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void queryPerson(String id) {
+        String url = "jdbc:sqlite:sqlite/JTP.db";
+
+        String sql = "select * FROM db.results WHERE id = \'" + id + "\';";
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String user = rs.getString("user");
+                String age = rs.getString("age");
+                String gender = rs.getString("gender");
+                String country = rs.getString("country");
+                String language = rs.getString("language");
+                String employment = rs.getString("employment");
+                String sensitivity = rs.getString("sensitivity");
+                String q1 = rs.getString("q1");
+                String q2 = rs.getString("q2");
+                String q3 = rs.getString("q3");
+                String q4 = rs.getString("q4");
+                String q5 = rs.getString("q5");
+                String q6 = rs.getString("q6");
+                String q7 = rs.getString("q7");
+
+
+                System.out.println(id + " | " + user + " | " + age +
+                        " | " + gender + " | " + country + " | " + language +
+                        " | " + employment + " | " + sensitivity + " | " + q1 +
+                        " | " + q2 + " | " + q3 + " | " + q4 + " | " + q5 +
+                        " | " + q6 + " | " + q7 + "\n");
+            }
+        } catch (SQLException e) {
+            JDBCTutorialUtilities.printSQLException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
     }
 }
