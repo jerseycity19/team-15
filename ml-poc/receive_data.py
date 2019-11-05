8dd3069681d29eed1500f9dea6df7df14a5cd93e
jmaloyjr <jmaloyjr@udel.edu>
2019-11-05 17:18:45 +0000

    update

--- ml-poc/receive_data.py
+++ ml-poc/receive_data.py
@@ -4,7 +4,7 @@ import xmltodict
 MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 mturk = boto3.client('mturk',
    aws_access_key_id = "",
-   aws_secret_access_key = "s",
+   aws_secret_access_key = "",
    region_name='us-east-1',
    endpoint_url = MTURK_SANDBOX
 )
